41144f0fc0488abc5fd2785efbdeb84e98cf5778
Sahil9309 <sahiltalwekar123@gmail.com>
2025-02-10 13:52:57 +0530

    react router crash course

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='w-full text-black text-center text-4xl font-bold padding: 6px' style={{backgroundColor:"orange"}}>React Router</h1>
+    </>
+  )
+}
+
+export default App
