68614f1c7e7e687cf01cb30893e694206a1c2943
Sahil9309 <sahiltalwekar123@gmail.com>
2025-05-28 11:41:16 +0530

    Fixed lint errors

--- 07reactRouter/src/components/About/About.jsx
+++ 07reactRouter/src/components/About/About.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function About() {
   return (
     <div className="py-16 bg-white">
@@ -13,16 +11,17 @@ export default function About() {
               Exploring the World of Software Coding
             </h2>
             <p className="mt-6 text-gray-600">
-              Hi, I'm Sahil, a passionate software developer with a strong focus
+              Hi, I&apos;m Sahil, a passionate software developer with a strong focus
               on C++, React JS, and web development. I enjoy building efficient
               algorithms, developing dynamic applications, and solving complex
               coding challenges. My expertise lies in front-end development and
-              creating interactive web applications. Whether it's optimizing
+              creating interactive web applications. Whether it&apos;s optimizing
               code for performance or crafting seamless user experiences, I love
               turning ideas into reality through code. This blog is where I
               share my insights, projects, and experiences in software
               development. Stay tuned for coding tips, tutorials, and deep dives
               into problem-solving techniques!
+
             </p>
             <p className="mt-4 text-gray-600">
               Step into a world of words where stories, thoughts, and life
---
+++ megaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>A Blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
